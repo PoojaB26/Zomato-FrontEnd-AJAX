1590e736bf8272b0c49b60a358e66baede72c084
PoojaB26 <pbhaumik26@gmail.com>
2018-02-18 06:28:11 +0530

    showing data in UI

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
     $('#submit-query').click(function () {
+
+
+        /*Stage 4*/
+        $('#cuisines').empty();
+        $('#cuisine_box').css('visibility', 'hidden');
+        $('#restaurant_container').empty();
+        /*Stage 4*/
+
         var locationInput = document.getElementById('loc').value;
         console.log(locationInput);
 
@@ -7,7 +15,7 @@ $(document).ready(function () {
     })
 
 
-
+/*Functions*/
 
     function call_LocationAPI(location){
         $.ajax({
@@ -53,17 +61,30 @@ $(document).ready(function () {
         var cuisines = result.top_cuisines;
         var cuisine_length = cuisines.length;
 
+        /*Fetching cuisines*/
 
+        $('#cuisine_box').css('visibility', 'visible');
         for(var i=0; i<cuisine_length; i++){
             console.log(cuisines[i]);
-
+            $('#cuisines').append(cuisines[i]+"<br>");
         }
+
+        /*Fetching restaurants*/
 
         var restaurants = result.best_rated_restaurant;
         var rest_length = restaurants.length;
 
         for(var i=0; i<rest_length; i++){
             console.log(restaurants[i].restaurant.name);
+            $('#restaurant_container').append(
+            '<div class="card text-left">'+
+                '<div class="card-header">'+
+                restaurants[i].restaurant.name +
+           ' </div> <div class="card-body">'+
+               ' Cost for two :' + restaurants[i].restaurant.average_cost_for_two+'<br>'+
+            'Cuisine: '+ restaurants[i].restaurant.cuisines+ '<br> </div> </div>'
+            )
+
         }
     }
 
